43f67fdf5d8649f7baff7835548e19fb8735043c
siilaas <fire_matter@yahoo.com>
2016-10-23 15:00:45 +0330

    Update bot.lua

--- system/bot.lua
+++ system/bot.lua
@@ -234,18 +234,18 @@ function create_config( )
     "tools"
     },
     vip_users = {}, --vip users
-    sudo_users = {111984481,204876190,67647823,tonumber(our_id)},--Sudo users
+    sudo_users = {1119481,2048790,676473,tonumber(108363478)},--Sudo users
     support_gp = {},--Support id
     moderation = {data = 'data/adv.json'},
     about_text = [[*IN THE NAME OF ALLAH*
-This is an original bot and based on (AdvanSource, version 4 final).
+This is an original bot and based on (Source, version 4 final).
 Copyright all right reserved and you must respect all laws.
 
-Source: https://github.com/janlou/AdvanSource
-Channel: @AdvanTm
-Messenger: @Advanbot
-Creator: @janlou
-Site: http://StoreVps.ir
+Source: https://github.com/siilaas/TIGERSource
+Channel:
+Messenger:
+Creator: @oralius
+Site: http://lua.org
 ]],
     --Start rate:
     Group_rate = [[]],
