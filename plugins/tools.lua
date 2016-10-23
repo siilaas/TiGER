f9773d753e9b21c5037ab9a0508a4d5af08759a5
siilaas <fire_matter@yahoo.com>
2016-10-23 23:20:29 +0330

    Update tools.lua

--- plugins/tools.lua
+++ plugins/tools.lua
@@ -229,10 +229,10 @@ function run(msg, matches)
 			   if not is_owner(msg) and not is_sudo(msg) then
 				   redis:setex("wait:"..msg.from.id, 30, true)
 				   redis:set("sticker:photo", "waiting")
-    	     return 'Please send your sticker now\n\nPowered by '..team..'\nJoin us : '..channel
+    	     return 'Please send your sticker now\n\nPowered by '..team..'\nJoin us :'
 				 end
     	redis:set("sticker:photo", "waiting")
-    	return 'Please send your sticker now\n\nPowered by '..team..'\nJoin us : '..channel
+    	return 'Please send your sticker now\n\nPowered by '..team..'\nJoin us :'
 			elseif redis:get("wait:"..msg.from.id) then
 			return "Please wait for 30 second."
 			end
@@ -241,10 +241,10 @@ function run(msg, matches)
 			   if not is_owner(msg) and not is_sudo(msg) then
 				   redis:setex("wait:"..msg.from.id, 30, true)
 				   redis:set("photo:sticker", "waiting")
-           return 'Please send your photo now\n\nPowered by '..team..'\nJoin us : '..channel
+           return 'Please send your photo now\n\nPowered by '..team..'\nJoin us :'
 				 end
       redis:set("photo:sticker", "waiting")
-      return 'Please send your photo now\n\nPowered by '..team..'\nJoin us : '..channel
+      return 'Please send your photo now\n\nPowered by '..team..'\nJoin us :'
 		  elseif redis:get("wait:"..msg.from.id) then
 			return "Please wait for 30 second."
 			end
